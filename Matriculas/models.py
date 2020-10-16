b420152daef565ca3fcf28ee4f24378bd56c3cb8
dcondo10 <dcondo@libelulasoft.com>
2020-10-15 22:09:27 -0500

    REFACTOR: definicion de Base de datos

--- Matriculas/models.py
+++ Matriculas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import JSONField
+
 from BackStrawBerryPy.models import BaseModel
 from Personas.models import Docente, Alumno
 
@@ -13,6 +15,11 @@ class PeriodoLectivo(BaseModel):
     fecha_fin_clases = models.DateField()
     observaciones = models.TextField(null=True, blank=True)
     responsables = models.ManyToManyField(Docente, through='ResponsablePeriodo')
+    '''
+        PREGUNTAS:
+            - Si hay responsables por periodo lectivo y cuantos son?
+        
+    '''
 
 
 class ResponsablePeriodo(BaseModel):
@@ -31,6 +38,14 @@ class Aula(BaseModel):
     docentes = models.ManyToManyField(Docente)
     periodo = models.ForeignKey(PeriodoLectivo, on_delete=models.CASCADE)
     observaciones = models.TextField(null=True, blank=True)
+    jornada = models.CharField(max_length=50, default='MATUTINA')
+
+    '''
+        PREGUNTAS:
+            - Tiene mas de una jornada?
+            - Cuanto docentes por aula?
+            - Los docentes dan la misma materia?
+    '''
 
 
 class Materia(BaseModel):
@@ -42,25 +57,80 @@ class Materia(BaseModel):
     objetivo = models.CharField(max_length=50)
     objetivo_especifico = models.CharField(max_length=50)
 
+    '''
+        PREGUNTAS:
+            - Informes que mandan al ministerior de educacion cuales? y formatos.
+                -  Malla de Alumnos?
+            
+    '''
+
 
 # MATRICULA
 class AlumnoAula(BaseModel):
     aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
-    nota_final = models.DecimalField(max_digits=6, decimal_places=2)
+    nota_final = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     numero_faltas = models.PositiveSmallIntegerField(default=0)
-    notas = models.ManyToManyField('Nota', through='NotaEstudiante')
     tratamiento = models.TextField()
     diagnostico = models.TextField()
-    matroicula = models.TextField()
+    matricula = models.TextField()
+    numero_matricula = models.CharField(max_length=155)
     aporte_voluntario = models.DecimalField(decimal_places=2, max_digits=6)
+    diagnostico_final = models.TextField()
+    faltas = JSONField()
+
+    '''
+        PREGUNTAS:
+            - Pierden el anio?
+                - Causas de perdida de anio?
+                - Numero maximo de matriculas?
+                - Motivos para perder el anio? 
+            
+            - Calculo para nota final(de todas las materias)
+            - Maximo de numero de faltas
+            - En caso de retirarce del IPCA que ocurre con la matricula?
+        
+        [
+            {
+                fecha: 2020-10-15,
+                horaInicio?: 08:00,
+                horaFin?: 10:00,
+                justificacion: "COMENTARIO DEL PADRE DE FAMILIA",
+                comentarios: "COMENTARIO DEL DOCENTE"   
+                usuario: obj             
+            }
+        ]
+            - Confirmar que la falta es por dia y no por horas.
+            
+        
+    '''
 
 
-class NotaEstudiante(BaseModel):
+# MallaAlumno
+class NotaMateria(BaseModel):
     alumno_aula = models.ForeignKey(AlumnoAula, on_delete=models.CASCADE)
-    nota = models.ForeignKey('Nota', on_delete=models.CASCADE)
-
-
-class Nota(BaseModel):
     valor = models.DecimalField(decimal_places=2, max_digits=6, default=0)
     observaciones = models.TextField(null=True, blank=True)
+    materia = JSONField()
+    # materia_fk = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    notas = JSONField(default=[])
+    '''
+    
+        PREGUNTAS:
+            - Formato de notas?
+            - Tipos de reportes que necesitan?
+            - Tienen minimos y maximo?
+            
+        matematica:
+            valorFinal: 100,
+            notas:[
+                {
+                    titulo:'Paseo1'
+                    valor:10
+                    fechaRegistro:2020-10-12-08:00:45,
+                    descripcion:'COMENTARIOS DOCENTE',
+                    usuario: objUser
+                }
+            ]
+            
+    '''
