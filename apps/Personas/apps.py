fb0cf733a4db6d319ed37885561eaae51a57f9bb
MrRainx <diegocondo1007@gmail.com>
2020-12-21 00:23:45 -0500

    REFACTOR: se cambio las validaciones para las tablas

--- apps/Personas/apps.py
+++ apps/Personas/apps.py
@@ -6,12 +6,16 @@ class PersonasConfig(AppConfig):
 
     def ready(self):
         from apps.Personas.models import FuncionPersonal
-        functiones_personal_default = [
-            FuncionPersonal(nombre='DOCENTE', codigo='docente'),
-            FuncionPersonal(nombre='SECRETARIA', codigo='secretaria'),
-            FuncionPersonal(nombre='CHOFER', codigo='chofer'),
-            FuncionPersonal(nombre='TERAPEUTA', codigo='terapeuta'),
-        ]
 
-        if functiones_personal_default.__len__() != FuncionPersonal.objects.count():
-            FuncionPersonal.objects.bulk_create(functiones_personal_default)
+        try:
+            functiones_personal_default = [
+                FuncionPersonal(nombre='DOCENTE', codigo='docente'),
+                FuncionPersonal(nombre='SECRETARIA', codigo='secretaria'),
+                FuncionPersonal(nombre='CHOFER', codigo='chofer'),
+                FuncionPersonal(nombre='TERAPEUTA', codigo='terapeuta'),
+            ]
+            if functiones_personal_default.__len__() != FuncionPersonal.objects.count():
+                FuncionPersonal.objects.bulk_create(functiones_personal_default)
+
+        except Exception as e:
+            pass
