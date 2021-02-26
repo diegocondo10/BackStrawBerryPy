b353d1c40f4f4ac96f6d7e9427e5e2d2a7fe069b
dCondo <dcondo@libelulasoft.com>
2021-02-25 19:01:48 -0500

    PROBLEMAS

--- BackStrawBerryPy/settings.py
+++ BackStrawBerryPy/settings.py
@@ -211,12 +211,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 AWS_ACCESS_KEY_ID = 'AKIAVECQLGORFTC7VVF3'
 AWS_SECRET_ACCESS_KEY = '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'
